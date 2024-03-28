23266ed6d356d203299e93b1f44760ce4dd9af0d
leoo1406 <158114824+leoo1406@users.noreply.github.com>
2024-03-28 13:56:20 -0300

    Delete alura_books directory

--- alura_books/src/componentes/UltimosLancamentos/dadosUltimosLancamentos.js
+++
@@ -1,8 +0,0 @@
-import livro from '../../imagens/livro.png';
-
-export const livros = [
-    {nome: 'Liderança em Design', id: 1, src:livro},
-    {nome: 'Caminho nas alturas', id: 2, src:livro},
-    {nome: 'Direção ao sucesso', id: 3, src:livro},
-    {nome: 'Seja um bom profissional', id: 4, src:livro},
-]
